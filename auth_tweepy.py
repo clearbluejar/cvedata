bdfb96ba85bcaf254a21cb0653aa6c734894660a
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2022-11-03 15:48:11 -0400

    cve twitter list updates

---
+++ auth_tweepy.py
@@ -0,0 +1,10 @@
+import os
+
+# Need to generate your tokens
+# https://developer.twitter.com/en/docs/authentication/faq
+
+os.environ["consumer_key"] = ""
+os.environ["consumer_secret"] = ""
+os.environ["bearer_token"] = ""
+os.environ["access_token"] = ""
+os.environ["access_token_secret"] = ""
--- build_researcher_twitter_lists.py
+++ build_researcher_twitter_lists.py
@@ -3,15 +3,20 @@ import os
 import pandas as pd
 from pandas import json_normalize
 import json
+from pathlib import Path
+import time
 
 from cvedata.acknowledgements import get_researcher_twitter_map_json
 
-auth = tweepy.OAuthHandler(os.getenv("consumer_key"), os.getenv("consumer_secret"))
-auth.set_access_token(os.getenv("access_token"), os.getenv("access_token_secret"))
+import auth_tweepy
 
-# rate limit issues https://improveandrepeat.com/2022/03/python-friday-114-debug-tweepy/
+auth = tweepy.OAuthHandler(os.getenv("consumer_key"),
+                           os.getenv("consumer_secret"))
+auth.set_access_token(os.getenv("access_token"),
+                      os.getenv("access_token_secret"))
 
-api = tweepy.API(auth)
+
+api = tweepy.API(auth, wait_on_rate_limit=True)
 client = tweepy.Client(
     bearer_token=os.getenv("bearer_token"),
     consumer_key=os.getenv("consumer_key"),
@@ -21,120 +26,81 @@ client = tweepy.Client(
     wait_on_rate_limit=True,
 )
 
-list_name = "researchers with a CVE"
-list_description = "A list of researchers that have earned at least 1 CVE"
+# Used to create list
+# list_name = "researchers with a CVE"
+# list_description = "A list of researchers that have earned at least 1 CVE"
 
 # twitter_list = api.create_list(name=list_name, description=list_description, mode='private')
 # list_id = twitter_list._json["id"]
 
 list_id = 1582329731005005825
-# list_meta = api.get_list(list_id = list_id)
-
-# print("Number of members before add_list_member() is used : " +
-#       str(api.get_list(list_id = list_id).member_count))
-      
 
 researchers_json = get_researcher_twitter_map_json()
+screen_name_to_researcher_map = {
+    str(v[0]).lower(): k for k, v in researchers_json.items()}
 
-screen_names  = set()
+
+screen_names = set()
 for researcher in researchers_json:
     for screen_name in researchers_json[researcher]:
         if screen_name:
             screen_names.add(screen_name)
 
+current_members_path = Path('current_members.json')
+current_members = []
+current_members_results = []
 
-current_members = client.get_list_members(list_id,user_auth=True).data
+if not current_members_path.exists():
+    #current_members = api.get_list_members(list_id=list_id)
+    for member in tweepy.Cursor(api.get_list_members, list_id=list_id).items():
+        current_members_results.append(member)
+    members_data = [member._json for member in current_members_results]
+    current_members_path.write_text(json.dumps(members_data))
+
+current_members = json.loads(current_members_path.read_text())
+
+members_df = pd.json_normalize(current_members)
+members_df['cvedata_name'] = members_df['screen_name'].apply(
+    lambda x: screen_name_to_researcher_map.get(str(x).lower()))
+print(members_df.head())
+members_df.to_json('members.json')
 
 current_ids = []
+current_screen_names = []
+
 for member in current_members:
-    current_ids.append(member.id)
+    current_ids.append(member['id'])
+    current_screen_names.append(member['screen_name'])
 
-    # if handle:
-    #     print(handle)
-    #     try:
-    #         user = client.get_user(username=handle, user_auth=True)
-        
-            
-    #         if user.data:
-    #             print(user.data['name'])
-    #             ids.append(user.data.data['id'])
-    #         else:
-    #             print(f"{handle} error: {user.errors}")
-    #     except Exception as e:
-    #         print("Error: {handle} {e}")
-    #         continue
+print(f"All screen names len {len(screen_names)}")
+screen_names_to_add = screen_names.difference(current_screen_names)
+screen_names_to_add = list(screen_names_to_add)
+print(f"Screen names to add len {len(screen_names_to_add)}")
+sub_size = 3
 
+print(f"Need to add {len(screen_names_to_add)} researchers")
+print(f"Starting member count = {len(current_screen_names)}")
 
-# for i in range(0, len(ids), 100):
-#     sub_ids = [id for id in ids[i : i + 100]]
-#     response = api.add_list_members(list_id=list_id, user_id=sub_ids)
-screen_names = list(screen_names)
-sub_size = 100
+member_count = len(current_screen_names)
+for i in range(0, len(screen_names_to_add), sub_size):
+    sub_screen_names = [sn for sn in screen_names_to_add[i: i + sub_size]]
 
-user_dfs = []
-
-# for i in range(0, len(screen_names), sub_size):
-#     sub_screen_names = [id for id in screen_names[i : i + sub_size]]
-#     response = api.add_list_members(list_id=list_id, screen_name=sub_screen_names, owner_screen_name='clearbluejar', slug=''1582329731005005825')
-    
-#     print("Number of members after add_list_member() is used : " +
-#       str(api.get_list(list_id = list_id).member_count))
-
-users_to_add = []
-
-for i in range(0, len(screen_names), sub_size):
-    sub_screen_names = [id for id in screen_names[i : i + sub_size]]
-    users = api.lookup_users(screen_name=sub_screen_names)
-    print(len(users))
-    
-    
-    for user in users:
-        resp = None
-
-        # create dataframe for user
-        user_dfs.append(json_normalize(user._json))
-
-        if user.id in current_ids:
-            print(f"Skipping {user.name}")
-        else:
-            print(f"Adding {user.name}")
-            users_to_add.append(user)
-
-
-print(f"Attempting to add {len(users_to_add)} researchers")
-for i in range(0, len(users_to_add), sub_size):
-    sub_screen_names = [id.screen_name for id in users_to_add[i : i + sub_size]]
-
+    # rate limit issues https://improveandrepeat.com/2022/03/python-friday-114-debug-tweepy/
+    # this section always suffers from Twitter strange rate limiting
+    # keep running until satisfied
     try:
-        resp = api.add_list_members(list_id=list_id, screen_name=sub_screen_names)
+        print(f"Adding members: {sub_screen_names}")
+        resp = api.add_list_members(
+            list_id=list_id, screen_name=sub_screen_names)
+        new_member_count = resp.member_count
+        print(
+            f"Added {new_member_count - member_count} of {len(sub_screen_names)} attempted")
+        member_count = new_member_count
+
+        # delete current members as file is not outdated
+        if current_members_path.exists():
+            current_members_path.unlink()
+
+        time.sleep(1)
     except Exception as e:
-        print(f"Failed {user.screen_name}")
         print(f"{e}")
-
-        
-                #resp = client.add_list_member(id=list_id, user_id=user.id)
-            #resp = api.add_list_member(list_id=list_id,id=user.id)
-            
-            # try:
-            #     #resp = api.add_list_member(list_id=list_id,screen_name=user.screen_name)
-            #     resp = client.add_list_member(id=list_id,user_id=user.id)
-            # except Exception as e:
-            #     print(f"Failed {user.screen_name}")
-            #     print(f"{e}")
-
-
-
-df = pd.concat(user_dfs,ignore_index=True)
-print(df.head(10))
-df.to_markdown('users.md')
-df.to_json('user.json')
-
-
-
-
-
-    #print(f"Add member succeeded: {response}")
-    # print("Number of members before add_list_member() is used : " +
-    #   str(api.get_list(list_id = list_id).member_count))
-
-print("done")
