5f64dc441910324c73937310d471eba423832a22
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2022-11-18 15:15:15 -0500

    flat download zip

--- cvedata/util.py
+++ cvedata/util.py
@@ -1,3 +1,7 @@
+import requests
+import shutil
+import zipfile
+import io
 import gzip
 import json
 from pathlib import Path
@@ -14,14 +18,13 @@ def get_file_json(path: Path, base: str) -> dict:
         else:
             with open(path) as f:
                 return json.load(f)
-    except FileNotFoundError as e:        
-        raise Exception(f"Missing {path}. \nPlease run {base} or download release data: python -m cvedata.download") from e
+    except FileNotFoundError as e:
+        raise Exception(
+            f"Missing {path}. \nPlease run {base} or download release data: python -m cvedata.download") from e
 
-import requests
-import io
-import zipfile
 
 # from https://techoverflow.net/2018/01/16/downloading-reading-a-zip-file-in-memory-using-python/
+
 
 def download_extract_zip_mem(url: str):
     """
@@ -38,7 +41,7 @@ def download_extract_zip_mem(url: str):
 
 # https://svaderia.github.io/articles/downloading-and-unzipping-a-zipfile/
 
-def download_extract_zip_to_path(url: str,path: Path):
+def download_extract_zip_to_path(url: str, path: Path):
     """
     Download zip file and extract to path
     """
@@ -47,3 +50,26 @@ def download_extract_zip_to_path(url: str,path: Path):
     assert response.status_code == 200
     with zipfile.ZipFile(io.BytesIO(response.content)) as thezip:
         thezip.extractall(path)
+
+
+def download_extra_zip_to_path_flat(url: str, path: Path):
+
+    # https://stackoverflow.com/questions/46954626/extract-zip-file-without-folder-python
+
+    response = requests.get(url)
+    assert response.status_code == 200
+    with zipfile.ZipFile(io.BytesIO(response.content)) as thezip:
+
+        for file in thezip.namelist():
+
+            # skip dirs
+            if file.endswith('/'):
+                continue
+
+            file_path = Path(path / Path(file).name)
+            print(f"Extracting {file} to {file_path}")
+
+            file_data_obj = thezip.open(file)
+
+            with file_path.open('wb') as f:
+                shutil.copyfileobj(file_data_obj, f)
