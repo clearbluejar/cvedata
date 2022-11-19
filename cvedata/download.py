acb002aeb56ed9ba01ff9b977bca2e5f1e31a486
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2022-11-18 23:23:01 -0500

    fix data zips

--- cvedata/download.py
+++ cvedata/download.py
@@ -3,7 +3,7 @@ import json
 from pathlib import Path
 
 from .config import DATA_DIR, CACHE_PATH, REPO_REL_VERSION_INFO, REPO_REL_DOWNLOAD_URL
-from .util import download_extra_zip_to_path_flat
+from .util import download_extract_zip_to_path
 
 RELEASE_DATA_DOWNLOAD_INFO = { 'url': REPO_REL_DOWNLOAD_URL + 'cvedata_data.zip', 'path': DATA_DIR }
 CACHE_DATA_DOWNLOAD_INFO = { 'url': REPO_REL_DOWNLOAD_URL + 'cvedata_cache.zip', 'path': CACHE_PATH }
@@ -37,13 +37,14 @@ def download_release_data():
 
     print(f"Downloading release data from: {RELEASE_DATA_DOWNLOAD_INFO['url']}")
 
-    download_extra_zip_to_path_flat(RELEASE_DATA_DOWNLOAD_INFO['url'],RELEASE_DATA_DOWNLOAD_INFO['path'])
+    download_extract_zip_to_path(RELEASE_DATA_DOWNLOAD_INFO['url'],RELEASE_DATA_DOWNLOAD_INFO['path'])
 
 def download_cache_data():
 
     print(f"Downloading release data from: {CACHE_DATA_DOWNLOAD_INFO['url']}")
 
-    download_extra_zip_to_path_flat(CACHE_DATA_DOWNLOAD_INFO['url'],CACHE_DATA_DOWNLOAD_INFO['path'])
+    download_extract_zip_to_path(CACHE_DATA_DOWNLOAD_INFO['url'],CACHE_DATA_DOWNLOAD_INFO['path'])
 
 if __name__ == "__main__":
     download_release_data()
+    download_cache_data()
