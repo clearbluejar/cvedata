630fb440c46168839ec994433fc879e4e31e794a
clearbluejar <3752074+clearbluejar@users.noreply.github.com>
2022-11-25 09:38:27 -0500

    nist test format

--- tests/test_nist.py
+++ tests/test_nist.py
@@ -2,15 +2,15 @@ from datetime import datetime
 import random
 import requests
 
-from cvedata.nist import get_all_nist_cve_urls,get_cves, NIST_OLDEST_YEAR
+from cvedata.nist import get_all_nist_cve_urls, get_cves, NIST_OLDEST_YEAR
 
 
 def test_nist_urls_available():
 
     urls = get_all_nist_cve_urls(NIST_OLDEST_YEAR)
 
-    for year,url in urls:
-        res = requests.head(url)
+    for year, url in urls:
+        res = requests.head(url, timeout=3)
         assert res.status_code == 200
 
 
@@ -20,12 +20,10 @@ def test_cves_from_all_years():
 
     cve_list = []
 
-    for year in range(NIST_OLDEST_YEAR,this_year):
+    for year in range(NIST_OLDEST_YEAR, this_year):
         for x in range(200):
             cve_list.append(f"CVE-{year}-{random.randint(0,9999)}")
 
     full_cves = get_cves(cve_list)
 
     assert len(cve_list) == len(full_cves)
-
-    
